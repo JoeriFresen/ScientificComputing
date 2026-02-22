3da799d38d57951df14b4652135acbbfcd7c0d0e
GrumbaBoi <maxvanbeusekom@icloud.com>
2026-02-22 19:08:20 +0100

    added comments and docstrings to diffusion

--- assignment_1/diffusion.py
+++ assignment_1/diffusion.py
@@ -4,21 +4,28 @@ import matplotlib.animation as animation
 import scipy.special
 
 def solve_diff(D, L, N, T, dt, t_arr=None, verbose=True, animate=False):
+    """
+    Solves the 2D diffusion equation using an explicit finite difference method.
+    """
     dx = L / N
     dt = min(dt, (0.25 * (dx**2 / D)))
     
     timesteps = T / dt
     c = np.zeros((N + 1, N + 1))
-     
+    
+    # initial condition: c(y=0) = 1, c(y=L) = 0
     c[0, :] = 1
     
     save_steps = {}
+    
+    # set t's to save at
     if t_arr is not None:
         for t in t_arr:
             index = int(t / dt)
             save_steps[index] = t
     snapshots = {} 
     
+    # for animation
     frames = []
     
     for t in range(int(timesteps) + 1):
@@ -32,7 +39,8 @@ def solve_diff(D, L, N, T, dt, t_arr=None, verbose=True, animate=False):
         if animate:
             if t % 10 == 0:
                 frames.append(c.copy())
-                
+        
+      
         y_u = np.roll(grid, -1, axis=0)
         y_d = np.roll(grid, 1, axis=0)
         x_r = np.roll(grid, -1, axis=1)
@@ -49,6 +57,9 @@ def solve_diff(D, L, N, T, dt, t_arr=None, verbose=True, animate=False):
             
         
 def analytic_sol(D, t, terms, y):
+    """
+    Computes the analytical solution for the diffusion equation at time t.
+    """
     c_analytical = np.zeros((y.shape[0], terms))
     for i in range(terms):
         er1 = (1 - y + 2 * i) / (2 * np.sqrt(D * t))
@@ -60,6 +71,10 @@ def analytic_sol(D, t, terms, y):
         
 
 def check_sol(D, L, N, T, dt, t_arr, terms):
+    """
+    Compares the numerical solution with the analytical solution at specified time points
+    and graphs the results.
+    """
     num_sol = solve_diff(D, L, N, T, dt, t_arr)
     dx = L / N
     y = np.arange(0, L + 1e-12, dx)
@@ -90,7 +105,7 @@ check_sol(1, 1, 100, 1, 0.1, t_arr, terms=10)
 snapshots = solve_diff(1, 1, 100, 1, 0.1, t_arr)
 
 
-# 2d domain for different t
+# 2d domain for different t - heatmap
 fig, ax = plt.subplots(2, 3, dpi=300, figsize=(12, 8), layout='constrained')
 axes = ax.flatten() 
 
@@ -114,21 +129,21 @@ plt.show()
 video_data = solve_diff(1, 1, 100, 1, 0.1, animate=True)
 
 
-# generate animated plot
-# fig, ax = plt.subplots()
-# im = ax.imshow(video_data[0], cmap='magma', origin='upper', animated=True, vmin=0, vmax=1)
-# ax.set_title("Diffusion Animation")
-# fig.colorbar(im, label="Concentration")
+#generate animated plot
+fig, ax = plt.subplots()
+im = ax.imshow(video_data[0], cmap='magma', origin='upper', animated=True, vmin=0, vmax=1)
+ax.set_title("Diffusion Animation")
+fig.colorbar(im, label="Concentration")
 
-# def update(i):
-#     im.set_data(video_data[i])
-#     ax.set_title(f"Time Step: {i*10}") 
-#     return [im]
+def update(i):
+    im.set_data(video_data[i])
+    ax.set_title(f"Time Step: {i*10}") 
+    return [im]
 
-# ani = animation.FuncAnimation(fig, update, frames=len(video_data), interval=30, blit=True)
-# ani.save('diffusion_heat.gif', writer='pillow', fps=30)
+ani = animation.FuncAnimation(fig, update, frames=len(video_data), interval=30, blit=True)
+ani.save('plots/diffusion_heat.gif', writer='pillow', fps=30)
 
-# plt.show()
+plt.show()
 
 
         
